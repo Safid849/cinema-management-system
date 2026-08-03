b8f0170922c5f4b8597b81d2e83e8e5e8e7cce70
valisoa01 <valisoatolotriniaina@gmail.com>
2026-08-03 11:08:11 +0300

    feat:add repository

---
+++ src/main/java/com/example/demo/cinema/repository/SeatRepository.java
@@ -0,0 +1,10 @@
+package com.example.demo.cinema.repository;
+
+import com.example.demo.cinema.entity.Seat;
+import java.util.List;
+import java.util.UUID;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface SeatRepository extends JpaRepository<Seat, UUID> {
+    List<Seat> findByRoomId(UUID roomId);
+}
